c532f3dab03c126f5d4b31acd119249d5a7f2f4a
Yvonne Chen <yvonnechen1991@gmail.com>
2021-01-13 11:04:56 -0600

    self-person

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,35 @@
 # your code goes here
+
+class Person
+
+    attr_accessor :bank_account
+
+    def initialize(name, bank_account= 25, happiness= 8, hygiene= 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end 
+
+    def name
+        @name
+    end 
+
+    # def happiness(happiness)   
+    #     happiness_1 = (0..10).min
+    #     happiness_2 = (0..10).max
+    #     @happiness = happiness_1 || happiness_2
+    # end 
+
+    def happiness
+        @happiness<= 0 && @happiness >= 10
+    end 
+
+
+
+    def hygiene
+        @hygiene = @hygiene
+    end 
+
+
+end 

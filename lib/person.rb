f7f0e0afb8596de7412f37d480ac25031e038116
HannaBM12 <Hannabmulugeta@gmail.com>
2021-01-13 13:35:39 -0500

    adding changes

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,8 @@
 # your code goes here
-
+require 'pry'
 class Person
 
-    attr_accessor :bank_account
+    attr_accessor :bank_account, :happiness, :hygiene
 
     def initialize(name, bank_account= 25, happiness= 8, hygiene= 8)
         @name = name
@@ -14,22 +14,66 @@ class Person
     def name
         @name
     end 
+    
+    def happiness=(happiness)
+        if happiness >=0 && happiness <= 10
+            @happiness = happiness
+        elsif happiness > 10
+            @happiness =10
+        else
+            @happiness =0
+        end
+    end
 
-    # def happiness(happiness)   
-    #     happiness_1 = (0..10).min
-    #     happiness_2 = (0..10).max
-    #     @happiness = happiness_1 || happiness_2
-    # end 
+    def hygiene=(hygiene)
+        if hygiene >=0 && hygiene <= 10
+            @hygiene = hygiene
+        elsif hygiene > 10
+            @hygiene =10
+        else
+            @hygiene =0
+        end
+    end
 
-    def happiness
-        @happiness<= 0 && @happiness >= 10
-    end 
+    def happy?
+        if @happiness > 7
+            return true
+        else
+            return false
+        end
+    end
+    
+    def clean?
+        if @hygiene > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def get_paid(salary)
+        @bank_account +=salary
+        "all about the benjamins"
+    end
 
 
+    def take_bath
+        if @hygiene < 7
+          @hygiene += 4
+        else
+          @hygiene = 10
+        end
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+      end
 
-    def hygiene
-        @hygiene = @hygiene
-    end 
-
-
+    # def take_bath(hygiene)
+    #     if hygiene >=0 && hygiene <=6
+    #         @hygiene = hygiene + 4
+    #     elsif hygiene > 10
+    #         @hygiene =10
+    #     elsif hygiene 
+    #         @hygiene =0
+    #     end
+    #     "♪ Rub-a-dub just relaxing in the tub ♫"
+    # end
 end 
